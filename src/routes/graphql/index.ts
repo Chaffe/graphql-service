402ea7207d37daa1a329db8cc4ab1864a3f46c08
Mikhail <mikhail.mudrogelov@gmail.com>
2025-11-23 00:56:02 +0300

    feat: init project

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -15,6 +15,7 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const { query, variables } = req.body;
       // return graphql();
     },
   });
