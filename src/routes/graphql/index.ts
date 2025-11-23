c89b5ddf95d3056098d69bfcb31efde2b88330b1
Mikhail <mikhail.mudrogelov@gmail.com>
2025-11-23 23:46:34 +0300

    feat: implement schema validation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,11 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import {
-  graphql,
-  GraphQLSchema,
-} from 'graphql';
+import {graphql, GraphQLSchema, parse, validate,} from 'graphql';
 import {QueryType} from "./types/query-type.js";
 import {MutationType} from "./types/mutation-type.js";
+import depthLimit from "graphql-depth-limit";
 
 const schema = new GraphQLSchema({ query: QueryType, mutation: MutationType });
 
@@ -23,6 +21,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
+
+      const errors = validate(schema, parse(query), [depthLimit(5)]);
+      if (errors.length) {
+        return { errors };
+      }
 
       return await graphql({
         schema,
